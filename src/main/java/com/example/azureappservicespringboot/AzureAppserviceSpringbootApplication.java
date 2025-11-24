53fc1b50478fe56249328d99449adbc2b48a375f
Ashraf Esmali (MAC) <ashrafemp@gmail.com>
2025-11-24 18:27:56 +0300

    Initial commit

---
+++ src/main/java/com/example/azureappservicespringboot/AzureAppserviceSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.azureappservicespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureAppserviceSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AzureAppserviceSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/azureappservicespringboot/AzureAppserviceSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.azureappservicespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureAppserviceSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
